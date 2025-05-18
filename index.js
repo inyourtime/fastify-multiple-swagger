6d7a1f6fa535c4fb54a476040b058c742bc182bf
Sahachai <inyourtimeguy@gmail.com>
2025-05-18 15:52:28 +0700

    feat: add decorator getDocumentSources (#9)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const fp = require('fastify-plugin')
+const { getPublishOptions } = require('./lib/utils')
 
 /**
  * @type {import('fastify').FastifyPluginCallback<>}
@@ -9,6 +10,8 @@ function plugin(fastify, opts, next) {
   if (!Array.isArray(opts.documents)) {
     return next(new TypeError('"documents" option must be an array'))
   }
+
+  const documentSources = []
 
   for (let index = 0; index < opts.documents.length; index++) {
     const documentOptions = opts.documents[index]
@@ -26,16 +29,45 @@ function plugin(fastify, opts, next) {
       defaultDecorator: opts.defaultDecorator,
     })
 
+    const publish = getPublishOptions(normalizedOptions.publish)
+    const jsonPath = typeof publish.json === 'string' ? publish.json : `/doc-${index}/json`
+    const yamlPath = typeof publish.yaml === 'string' ? publish.yaml : `/doc-${index}/yaml`
+
     // Register route for json/yaml
     fastify.register(require('./lib/route'), {
       ...opts,
       prefix: opts.routePrefix,
-      ...normalizedOptions,
-      documentIndex: index,
+      publish,
+      jsonPath,
+      yamlPath,
+      decorator: normalizedOptions.decorator,
     })
+
+    const documentSource = {
+      decorator: normalizedOptions.decorator,
+      json: publish.json
+        ? opts.routePrefix
+          ? withPrefix(opts.routePrefix, jsonPath)
+          : jsonPath
+        : null,
+      yaml: publish.yaml
+        ? opts.routePrefix
+          ? withPrefix(opts.routePrefix, yamlPath)
+          : yamlPath
+        : null,
+    }
+
+    documentSources.push(documentSource)
   }
 
+  fastify.decorate('getDocumentSources', () => documentSources)
+
   next()
+}
+
+function withPrefix(routePrefix, url) {
+  const prefix = routePrefix.endsWith('/') ? routePrefix.slice(0, -1) : routePrefix
+  return `${prefix}${url}`
 }
 
 /** exports */
