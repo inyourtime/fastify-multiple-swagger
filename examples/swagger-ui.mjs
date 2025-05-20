fd22db729ac70bc93e94863f43b21261801a7850
inyourtime <inyourtimeguy@gmail.com>
2025-05-20 20:53:19 +0700

    add examples

---
+++ examples/swagger-ui.mjs
@@ -0,0 +1,47 @@
+import Fastify from 'fastify'
+import SwaggerUI from '@fastify/swagger-ui'
+import fastifyMultipleSwagger from '../index.js'
+
+const app = Fastify()
+
+const internalOption = {
+  decorator: 'internal',
+  swaggerOptions: {
+    openapi: {
+      info: {
+        title: 'Internal',
+        version: '1.0.0',
+      },
+    },
+  },
+  name: 'Internal',
+}
+
+const externalOption = {
+  decorator: 'external',
+  swaggerOptions: {
+    openapi: {
+      info: {
+        title: 'External',
+        version: '1.0.0',
+      },
+    },
+  },
+  name: 'External',
+}
+
+await app.register(fastifyMultipleSwagger, {
+  documents: [internalOption, externalOption],
+})
+
+await app.register(SwaggerUI, {
+  routePrefix: '/reference',
+  uiConfig: {
+    urls: app.getDocumentSources({ swaggerUI: true }),
+  },
+})
+
+app.register(import('./routes/internal.mjs'), { prefix: '/internal' })
+app.register(import('./routes/external.mjs'), { prefix: '/external' })
+
+await app.listen({ port: 3000 })
